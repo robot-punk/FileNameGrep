4dbaa2a5b59d68a4c9c95598826465202072228b
robot-punk <he_meets_no_band@yahoo.co.jp>
2015-11-22 02:12:54 +0900

    現時点のバックアップファイルと比較するようにした
    過去のファイルリストではなく
    現時点のバックアップ対象ディレクトリと
    現時点のパックアップされているディレクトリとを
    比較するようにした

--- DiffFileCopy/DiffFileCopy.py
+++ DiffFileCopy/DiffFileCopy.py
@@ -16,11 +16,12 @@ from datetime import datetime
 import difflib
 import filecmp
  
-# 現在のファイル一覧を作成する
-def write_now_file_list(backup_src_path, file_name):
-    print backup_src_path
-    target_files = IOUtil.get_all_files(backup_src_path)
-#    write_file_list(file_name)
+# 指定されたディレクトリパスのファイルのリストを
+# 指定されたファイルに書き込む
+def create_file_list(dir_path, file_name):
+    # print dir_path
+    # print file_name
+    target_files = IOUtil.get_all_files(dir_path)
     with open(file_name, 'w') as f:
         for f_name in target_files:
             f.writelines(f_name + '\n')
@@ -29,11 +30,13 @@ def write_now_file_list(backup_src_path, file_name):
 # ファイルをローテートする
 # return: ローテート後のファイル名
 def file_lotate(file_name):
+    if (not os.path.exists(file_name)):
+        return
+
     src = file_name
     today = datetime.today()
     dst = file_name + '_' + str(today.year) + str(today.month) + str(today.day - 1)
     shutil.copyfile(src, dst)
-    return dst
     
 # 設定ファイルの値を取得する。
 # 指定ノードの値を取得する。複数ある場合は最初に見つかったものを返す。
@@ -42,38 +45,46 @@ def get_first_node_config(node_name):
     tree = parse('./config.xml')
     return tree.getroot().find(node_name).text
 
-def main():
-    # ファイルリストをローテーションする。
-    file_name = 'log/file_list'
-    if (not os.path.exists(file_name)):
-        f = open(file_name, 'w')
-        f.close()
-
-    before_file = file_lotate(file_name)
-
-    # 現在のファイル一覧を作成する。
-    backup_src_path = get_first_node_config('backup_src_path')
-    write_now_file_list(backup_src_path, file_name)
-
-    # 現在と過去のファイルの差分を取得する。
+def add_time(value):
+    today = datetime.today()
+    return value + '_' + datetime.now().strftime("%Y%m%d_%H%M%S")
+    
+def get_diff_list(before, now):
     result = []
     d = difflib.Differ()
-    with open(before_file) as before_file_list:
-        with open(file_name) as now_file_list:
+    with open(before) as before_file_list:
+        with open(now) as now_file_list:
             result  = list(d.compare(before_file_list.read().splitlines(1),
                                     now_file_list.read().splitlines(1)))
+    
+    return result
+            
+    
+def main():
+
+    # 前回バックアップされたファイルの一覧を取得する。
+    backup_dst_path = get_first_node_config('backup_dst_path')
+    dst_list_file_name = add_time('log/dst_file_list')
+    # file_lotate(dst_list_file_name)
+    create_file_list(backup_dst_path, dst_list_file_name)
+    
+    # 現在のファイル一覧を作成する。
+    backup_src_path = get_first_node_config('backup_src_path')
+    src_list_file_name = add_time('log/src_file_list')
+    # file_lotate(src_list_file_name)
+    create_file_list(backup_src_path, src_list_file_name)
+
+    # 現在と前回のファイルリストの差分を取得する。
+    result = get_diff_list(dst_list_file_name, src_list_file_name)
 
 
 
     # 増えたファイルをバックアップフォルダにコピーする。
-    # TODO 行頭に+ がついた行のファイルのみをコピーする。
     for line in result:
         if '+' in line:
             src_file = line.lstrip('+ ').rstrip()
-            print src_file
             dst_file = src_file.replace(get_first_node_config('backup_src_path'),
                                         get_first_node_config('backup_dst_path'))
-            print dst_file
             
             # ファイルを作成する
             dst_dir = os.path.dirname(dst_file)
@@ -81,7 +92,7 @@ def main():
                 os.makedirs(os.path.dirname(dst_file))
                 
             shutil.copy(src_file, dst_file)
-            
+            print line
 
 if __name__ == '__main__':
     sys.exit(main())
